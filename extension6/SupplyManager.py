73389ee5ae540c9f502ea2660fb3bf9842a5d1cc
aswejei <nikitaklinchuk@gmail.com>
2022-11-09 19:43:54 +0300

    fixed some bugs in SupplyManager class

--- extension6/SupplyManager.py
+++ extension6/SupplyManager.py
@@ -1,15 +1,15 @@
 import time
 
-from root.CoffeeHouse import CoffeeHouse
+from extension6.CoffeeHouseWithSupplyManager import CoffeeHouseWithSupplyManager
 from root.Ingredient import Ingredient
 from root.Product import Product
 
 
 class SupplyManager:
-    _model: CoffeeHouse
+    _model: CoffeeHouseWithSupplyManager
     _state: bool
 
-    def __init__(self, model: CoffeeHouse):
+    def __init__(self, model: CoffeeHouseWithSupplyManager):
         self._model = model
         self._state = True
 
@@ -17,11 +17,13 @@ class SupplyManager:
         self._state = False
         time.sleep(amount/10)
         self._model.addIngredient(ingredient, amount)
+        self._state = True
 
     def renewProduct(self, product: Product, amount: int) -> None:
         self._state = False
         time.sleep(amount/10)
         self._model.addDessert(product, amount)
+        self._state = True
 
     def getManagerState(self) -> bool:
         return self._state
